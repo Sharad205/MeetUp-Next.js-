c99dab49998ab8c50ed079d0de96f5a0252bbb7d
Sharad205 <sharadvala205@gmail.com>
2021-04-27 16:51:19 +0530

    Name setup done

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function HomePage(props) {
 	return (
 		<Fragment>
 			<Head>
-				<title>React Meetups</title>
+				<title>Yr Meetups</title>
 				<meta
 					name="description"
 					content="Browse a huge list of highly active React meetups!"
